27c9fee6b1d979fa15d9e2e2e8bd15f971dbef09
Zura014 <zuragagnidze50@gmail.com>
2024-10-13 23:46:43 +0400

    done for today

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -2,12 +2,14 @@ import { Component } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { register } from '../../store/actions';
+import { RegisterRequestInterface } from '../../types/registerRequest.interface';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'mc-register',
   templateUrl: './register.component.html',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, RouterLink],
 })
 export class RegisterComponent {
   form = this.fb.nonNullable.group({
@@ -19,6 +21,10 @@ export class RegisterComponent {
   constructor(private fb: FormBuilder, private store: Store) {}
 
   onSubmit(): void {
-    this.store.dispatch(register());
+    const request: RegisterRequestInterface = {
+      user: this.form.getRawValue(),
+    };
+
+    this.store.dispatch(register({ request }));
   }
 }
