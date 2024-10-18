4beb7d11ed6a2401a4cb888d5966778038194a9f
Zura014 <zuragagnidze50@gmail.com>
2024-10-18 23:57:21 +0400

    2 streams in one

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -5,9 +5,13 @@ import { RegisterRequestInterface } from '../../types/registerRequest.interface'
 import { RouterLink } from '@angular/router';
 import { AuthStateInterface } from '../../types/authState.interface';
 import { CommonModule } from '@angular/common';
-import { selectIsSubmitting, selectValidationErrors } from '../../store/reducers';
+import {
+  selectIsSubmitting,
+  selectValidationErrors,
+} from '../../store/reducers';
 import { AuthService } from '../../services/auth.service';
 import { authActions } from '../../store/actions';
+import { combineLatest } from 'rxjs';
 
 @Component({
   selector: 'mc-register',
@@ -21,8 +25,11 @@ export class RegisterComponent {
     email: ['', [Validators.required, Validators.email]],
     password: ['', [Validators.required]],
   });
-  isSubmitting$ = this.store.select(selectIsSubmitting);
-  backendErrors$ = this.store.select(selectValidationErrors);
+
+  data$ = combineLatest({
+    isSubmitting$: this.store.select(selectIsSubmitting),
+    backendErrors$: this.store.select(selectValidationErrors),
+  });
 
   constructor(
     private fb: FormBuilder,
