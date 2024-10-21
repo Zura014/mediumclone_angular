9aff45c698ff5453357a7d5516dc6c2de93e6899
Zura014 <zuragagnidze50@gmail.com>
2024-10-21 15:31:45 +0400

    backend errors done

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -3,13 +3,11 @@ import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { RegisterRequestInterface } from '../../types/registerRequest.interface';
 import { RouterLink } from '@angular/router';
-import { AuthStateInterface } from '../../types/authState.interface';
 import { CommonModule } from '@angular/common';
 import {
   selectIsSubmitting,
   selectValidationErrors,
 } from '../../store/reducers';
-import { AuthService } from '../../services/auth.service';
 import { authActions } from '../../store/actions';
 import { combineLatest } from 'rxjs';
 import { BackendErrorMessages } from 'src/app/shared/components/backendErrorMessages/backendErrorMessages.component';
@@ -27,30 +25,22 @@ import { BackendErrorMessages } from 'src/app/shared/components/backendErrorMess
 })
 export class RegisterComponent {
   form = this.fb.nonNullable.group({
-    username: ['', [Validators.required]],
-    email: ['', [Validators.required, Validators.email]],
-    password: ['', [Validators.required]],
+    username: ['', Validators.required],
+    email: ['', Validators.required],
+    password: ['', Validators.required],
   });
-
   data$ = combineLatest({
     isSubmitting: this.store.select(selectIsSubmitting),
     backendErrors: this.store.select(selectValidationErrors),
   });
 
-  constructor(
-    private fb: FormBuilder,
-    private store: Store<{ auth: AuthStateInterface }>,
-    private authService: AuthService
-  ) {}
+  constructor(private fb: FormBuilder, private store: Store) {}
 
-  onSubmit(): void {
+  onSubmit() {
+    console.log('form', this.form.getRawValue());
     const request: RegisterRequestInterface = {
       user: this.form.getRawValue(),
     };
-
     this.store.dispatch(authActions.register({ request }));
-    this.authService
-      .register(request)
-      .subscribe((res) => console.log('res', res));
   }
 }
