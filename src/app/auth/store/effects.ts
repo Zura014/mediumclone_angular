a1798ccaf856272d0d879578a8791e3afe92d546
zura014 <zuragagnidze50@gmail.com>
2024-11-03 22:30:08 +0400

    login store

--- src/app/auth/store/effects.ts
+++ src/app/auth/store/effects.ts
@@ -34,10 +34,47 @@ export const registerEffect = createEffect(
   { functional: true }
 );
 
+export const loginEffect = createEffect(
+  (
+    actions$ = inject(Actions),
+    authService = inject(AuthService),
+    persistanceService = inject(PersistanceService)
+  ) => {
+    return actions$.pipe(
+      ofType(authActions.login),
+      switchMap(({ request }) => {
+        return authService.login(request).pipe(
+          map((currentUser: CurrentUserInterface) => {
+            persistanceService.set('accessToken', currentUser.token);
+            return authActions.loginSuccess({ currentUser });
+          }),
+          catchError((err: HttpErrorResponse) => {
+            return of(authActions.loginFailure({ errors: err.error.errors }));
+          })
+        );
+      })
+    );
+  },
+  { functional: true }
+);
+
 export const redirectAfterRegisterEffect = createEffect(
   (actions$ = inject(Actions), router = inject(Router)) => {
     return actions$.pipe(
       ofType(authActions.registerSuccess),
+      tap(() => router.navigateByUrl('/'))
+    );
+  },
+  {
+    functional: true,
+    dispatch: false,
+  }
+);
+
+export const redirectAfterLoginEffect = createEffect(
+  (actions$ = inject(Actions), router = inject(Router)) => {
+    return actions$.pipe(
+      ofType(authActions.loginSuccess),
       tap(() => router.navigateByUrl('/'))
     );
   },
