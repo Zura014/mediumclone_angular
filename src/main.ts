8b349e79f238d5d4494e95331fbbf3a78f8f9d09
Zura014 <zuragagnidze50@gmail.com>
2024-11-26 21:30:44 +0400

    getting and listing feed data

--- src/main.ts
+++ src/main.ts
@@ -9,8 +9,13 @@ import { authFeatureKey, authReducer } from './app/auth/store/reducers';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideEffects } from '@ngrx/effects';
 import * as authEffects from './app/auth/store/effects';
+import * as feedEffects from './app/shared/components/feed/store/effects';
 import { provideRouterStore, routerReducer } from '@ngrx/router-store';
 import { authInterceptor } from './app/shared/services/authInterceptor';
+import {
+  feedFeatureKey,
+  feedReducer,
+} from './app/shared/components/feed/store/reducers';
 
 bootstrapApplication(AppComponent, {
   providers: [
@@ -20,7 +25,9 @@ bootstrapApplication(AppComponent, {
     }),
     provideRouterStore(),
     provideState(authFeatureKey, authReducer),
+    provideState(feedFeatureKey, feedReducer),
     provideEffects(authEffects),
+    provideEffects(feedEffects),
     provideStoreDevtools({
       maxAge: 25,
       logOnly: !isDevMode(),
