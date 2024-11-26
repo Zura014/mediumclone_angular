8d0d5fcf8de97e6f8cdbaa74c03e09facb0dc873
Zura014 <zuragagnidze50@gmail.com>
2024-11-26 22:08:38 +0400

    feed pagination done

--- src/app/shared/components/feed/feed.component.ts
+++ src/app/shared/components/feed/feed.component.ts
@@ -4,18 +4,30 @@ import { feedActions } from './store/actions';
 import { combineLatest } from 'rxjs';
 import { selectError, selectFeedData, selectIsLoading } from './store/reducers';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
+import { RouterLink, Router, ActivatedRoute, Params } from '@angular/router';
 import { ErrorMessageComponent } from '../errorMessage/errorMessage.component';
 import { LoadingComponent } from '../loading/loading.component';
+import { environment } from 'src/environments/environment.development';
+import { PaginationComponent } from '../pagination/pagination.component';
+import queryString from 'query-string';
 
 @Component({
   selector: 'mc-feed',
   templateUrl: './feed.component.html',
-  imports: [CommonModule, RouterLink, ErrorMessageComponent, LoadingComponent],
+  imports: [
+    CommonModule,
+    RouterLink,
+    ErrorMessageComponent,
+    LoadingComponent,
+    PaginationComponent,
+  ],
   standalone: true,
 })
 export class FeedComponent implements OnInit {
   @Input() apiUrl: string = '';
+  limit: number = environment.limit;
+  baseUrl = this.router.url.split('?')[0];
+  currentPage: number = 0;
 
   data$ = combineLatest({
     isLoading: this.store.select(selectIsLoading),
@@ -23,9 +35,29 @@ export class FeedComponent implements OnInit {
     feed: this.store.select(selectFeedData),
   });
 
-  constructor(private store: Store) {}
+  constructor(
+    private store: Store,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
     this.store.dispatch(feedActions.getFeed({ url: this.apiUrl }));
+    this.route.queryParams.subscribe((params: Params) => {
+      this.currentPage = +params['page'] || 0;
+      this.fetchFeed();
+    });
+  }
+
+  fetchFeed(): void {
+    const offset = this.currentPage * this.limit - this.limit;
+    const parsedUrl = queryString.parseUrl(this.apiUrl);
+    const stringifiedParams = queryString.stringify({
+      limit: this.limit,
+      offset,
+      ...parsedUrl.query,
+    });
+    const apiUrlWithParams = `${parsedUrl.url}?${stringifiedParams}`;
+    this.store.dispatch(feedActions.getFeed({ url: apiUrlWithParams }));
   }
 }
