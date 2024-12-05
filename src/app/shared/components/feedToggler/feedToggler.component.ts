def3085d05dc307a3cce76916022c240ffa8002b
Zura014 <zuragagnidze50@gmail.com>
2024-12-05 23:55:42 +0400

    feedToggler added

---
+++ src/app/shared/components/feedToggler/feedToggler.component.ts
@@ -0,0 +1,19 @@
+import { AsyncPipe, NgIf } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { selectCurrentUser } from 'src/app/auth/store/reducers';
+
+@Component({
+  selector: 'mc-feed-toggler',
+  templateUrl: './feedToggler.component.html',
+  standalone: true,
+  imports: [NgIf, RouterLink, RouterLinkActive, AsyncPipe]
+})
+export class FeedTogglerComponent {
+  @Input() tagName?: string;
+
+  currentUser$ = this.store.select(selectCurrentUser);
+
+  constructor(private store: Store) {}
+}
