d77f845eda3cf5603894026a8a2171670e0feb82
Zura014 <zuragagnidze50@gmail.com>
2024-12-18 15:47:44 +0400

    articleForm

--- src/app/createArticle/components/createArticle/createArticle.component.ts
+++ src/app/createArticle/components/createArticle/createArticle.component.ts
@@ -1,8 +1,22 @@
 import { Component } from '@angular/core';
+import { ArticleFormValuesInterface } from 'src/app/shared/components/articleForm/types/articleFormValues.interface';
+import { ArticleFormComponent } from "../../../shared/components/articleForm/articleForm.component";
 
 @Component({
   selector: 'mc-create-article',
   templateUrl: './createArticle.component.html',
   standalone: true,
+  imports: [ArticleFormComponent],
 })
-export class CreateArticleComponent {}
+export class CreateArticleComponent {
+  initialValues = {
+    title: '',
+    description: '',
+    body: '',
+    tagList: [],
+  };
+
+  onSubmit(articleFormValues: ArticleFormValuesInterface): void {
+    console.log('onSubmit in create article', articleFormValues);
+  }
+}
